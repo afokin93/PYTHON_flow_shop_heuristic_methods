e95929ae636f90eecf2c6167379fb635a831114a
AndreCouto11 <115991511+AndreCouto11@users.noreply.github.com>
2023-12-07 18:16:22 +0000

    ajaja

--- solverFLOW.py
+++ solverFLOW.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import TextIO, Optional, Any
-import sys
+import sys , random, operator, time
 
 import Flow as flow
 
@@ -47,18 +47,58 @@ def first_improvement(s):
         v = next(vi, None) 
     return s
 
+def greed_construction_random_tie_breaking(problem):
+    s = problem.empty_solution()
+    ci = s.add_moves() #todas as componentes que podemos add
+    c = next(ci, None) #considerar a 1a, se nõa houver usar none, solucao vazia
+    while c is not None:
+        best_c, best_incr = [c], s.lower_bound_incr_add(c) #melhor incremento
+        for c in ci:
+            incr = s.lower_bound_incr_add(c)
+            if incr < best_incr: # problema de minimização, daí querer ue seja melhor
+                best_c, best_incr = [c], incr #atualiza
+            elif incr == best_incr: #caso empate, aleatorio
+                best_c.append(c)
+        s.add(random.choice(best_c))
+        ci = s.add_moves()
+        c = next(ci, None)
+    return s
+
+def greed_randomize_adaptive_construction(problem, alpha=0): #aqui vamos por numa lista para ir comparando os valores, mais flexibilidade, alfa pequeno gera valores melhores
+    s = problem.empty_solution()
+    cl = [(s.lower_bound_incr_add(c), c) for c in s.add_moves()] #candidate list e enumera elas
+    while len(cl) != 0:
+        c_min = min(cl, key=operator.itemgetter(0))[0]
+        c_max = max(cl, key=operator.itemgetter(0))[0]
+        thresh = c_min + alpha * (c_max - c_min)
+        rcl = [c for incr, c in cl if incr <= thresh] #restrict candidate list, todas as componentes para
+        s.add(random.choice(rcl))
+        cl = [(s.lower_bound_incr_add(c), c) for c in s.add_moves()] #reinicia cl para o while continuar
+    return s
+
+def grasp(problem, budget, alpha=0):
+    start = time.perf_counter()
+    best_s = greed_randomize_adaptive_construction(problem, alpha) #melhor solucao é a primeira
+    best_obj = best_s.objective()
+    while time.perf_counter() - start < budget: #enquanto nao tiver passado tempo o sudifiente
+        s = greed_randomize_adaptive_construction(problem, alpha)
+        obj = s.objective() #valor objetivo
+        if obj < best_obj:
+            best_s, best_obj = s, obj
+            print(obj)
+    return best_s
 
 if __name__ == '__main__':
     prob = flow.Problem.from_textio(sys.stdin) #instanciar o problema
     sol1 = greed_construction(prob)
     print("Obj:", "{:.2f}".format(sol1.objective()), 'Sol:', sol1)
-    #sol2 = greed_construction_random_tie_breaking(prob)
-    #print("Obj:", "{:.2f}".format(sol2.objective()), 'Sol:', sol2)
-    #sol3 = greed_randomize_adaptive_construction(prob, 0.01) #alfa pequeno melhor
-    #print("Obj:", "{:.2f}".format(sol3.objective()), 'Sol:', sol3)
-    #sol4 = grasp(prob, 2, 0.01)
-    #print("Obj:", "{:.2f}".format(sol4.objective()), 'Sol:', sol4)
+    sol2 = greed_construction_random_tie_breaking(prob)
+    print("Obj:", "{:.2f}".format(sol2.objective()), 'Sol:', sol2)
+    sol3 = greed_randomize_adaptive_construction(prob, 0.01) #alfa pequeno melhor
+    print("Obj:", "{:.2f}".format(sol3.objective()), 'Sol:', sol3)
+    sol4 = grasp(prob, 2, 0.01)
+    print("Obj:", "{:.2f}".format(sol4.objective()), 'Sol:', sol4)
     sol5 = best_improvement(prob.empty_solution())
-    print("Obj:", "{:.2f}".format(sol5.objective()), 'Sol:', sol1.copy())
+    print("Obj:", "{:.2f}".format(sol5.objective()), 'Sol:', sol5)
     sol6 = first_improvement(prob.empty_solution())
-    print("Obj:", "{:.2f}".format(sol6.objective()), 'Sol:', sol1.copy())
+    print("Obj:", "{:.2f}".format(sol6.objective()), 'Sol:', sol6)
